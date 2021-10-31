3b1d6f7a9c1fcdc9289b1c48053569bd9c1c275e
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2021-11-01 00:02:14 +0900

    UI class created

--- app.js
+++ app.js
@@ -6,3 +6,48 @@ class Book {
     this.isbn = isbn;
   }
 }
+
+// UI class: Handle UI Tasks
+class UI {
+  static displayBook() {
+    const StoredBooks = [
+      {
+        title: 'Book One',
+        author: 'John Doe',
+        isbn: '3434434'
+      },
+      {
+        title: 'Book Two',
+        author: 'Jane Doe',
+        isbn: '45545'
+      }
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class='btn btn-danger btn-sm delete'>X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
+
+// Store Class: Handle Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
