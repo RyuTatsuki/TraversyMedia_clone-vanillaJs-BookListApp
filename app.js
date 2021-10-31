c467fea309fd4439d82901f3ff027f5075fd4ec7
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2021-11-01 01:49:25 +0900

    addBook method created

--- app.js
+++ app.js
@@ -81,7 +81,9 @@ class Store {
   };
 
   static addBook(book) {
-
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
   };
 
   static removeBook(isbn) {
